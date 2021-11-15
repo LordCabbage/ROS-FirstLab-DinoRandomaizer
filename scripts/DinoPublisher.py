4b03e24c2f9a8a435844f49f6c27c2c0a5f91c87
LordCabbage <dkkapustin99@gmail.com>
2021-11-15 19:26:22 +0300

    Release

--- scripts/DinoPublisher.py
+++ scripts/DinoPublisher.py
@@ -2,17 +2,9 @@
 
 import sys
 import rospy
-import random
 
 from ros_lab_dino.srv import *
-
-family = ['Saurischia','Herrerasauridae','Eoraptor', 'Sauropodomorpha', ' Theropoda',
- 'Ornithischia', 'Pisanosaurus','Heterodontosauridae','Thyreophora','Cerapoda']
-period = ['Triassic', 'Jurassic', 'Cretaceous']
-
-
-x = random.randint(0,9)
-y = random.randint(0,2)
+from std_msgs.msg import String
 
 def dino_facts_client(a):
     rospy.wait_for_service('dino_facts')
@@ -32,5 +24,9 @@ if __name__ == '__main__':
     else:
         print(usage())
         sys.exit(1)
-    print("Requesting facts about %s"%a)
-    print("%s belonged to the %s family and lived in the %s"%(dino_facts_client(a),family[x],period[y]))
+    pub = rospy.Publisher('fgiver', String, queue_size=10)
+    rospy.init_node('DinoPublisher', anonymous=True)
+    name = dino_facts_client(a)
+    print("Requesting facts about %s"%name)
+    print("Fact about %s is ready"%name)
+    pub.publish(name)
--- scripts/DinoSubscriber.py
+++ scripts/DinoSubscriber.py
@@ -3,23 +3,34 @@
 import rospy
 from ros_lab_dino.srv import *
 import random
+from std_msgs.msg import String
 
 suffixes = ['saurus', 'raptor', 'pteryx', 'stacator',
 'rex', 'ceratops', 'gnathus', 'roides', 'draco', 'dromeus']
+family = ['Saurischia','Herrerasauridae','Eoraptor', 'Sauropodomorpha', ' Theropoda',
+ 'Ornithischia', 'Pisanosaurus','Heterodontosauridae','Thyreophora','Cerapoda']
+period = ['Triassic', 'Jurassic', 'Cretaceous']
 
 
 def return_dino_facts(req):
-    print("Returning fact about %s"%(req.a))
+    print("Returning real name of %s"%(req.a))
     L = list(req.a)
     z = random.randint(0,9)
     L[0] = L[0].upper()
     b = ''.join(L) + suffixes[z]
     return DinoFactsResponse(b)
 
+def factback(data):
+    x = random.randint(0,9)
+    y = random.randint(0,2)
+    print("%s belonged to the %s family and lived in the %s"%(data.data,family[x],period[y]))
+
 def dino_facts_server():
-    rospy.init_node('dino_facts_server')
+    rospy.init_node('DinoSubscriber',anonymous=True)
     f = rospy.Service('dino_facts', DinoFacts, return_dino_facts)
     print("Ready to give you true facts")
+    rospy.Subscriber("fgiver",String,factback)
     rospy.spin()
+
 if __name__ == "__main__":
     dino_facts_server()
